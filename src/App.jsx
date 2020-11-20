0a4749aebd4cb2a815f572753adc8814e6845384
Frank McNulty <frank.mcnulty@nology.io>
2020-11-20 17:38:08 +0000

    edits

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import SubmitForm from './components/SubmitForm/SubmitForm';
 
 class App extends Component {
   state = {
-    tasks: ['task 1', 'task 2', 'task 3'],
+    tasks: [],
     user: {}
   }
   componentDidMount() {
